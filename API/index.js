eee7475f71026997a62148e49040e06cbaf64c11
J.A Venura Jayaasingha <142327713+VenuraSLiit@users.noreply.github.com>
2024-01-25 09:04:12 +0530

    database connected

--- API/index.js
+++ API/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
+mongoose.connect('mongodb+srv://venurajayasingha1:gayasha123@cluster0.fjkwkzx.mongodb.net/?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("Database connected");
+})
 const app = express();
 const port = 3001;
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
